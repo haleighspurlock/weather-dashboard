f0ddfe957031efb64cf1a8926ba0131bb9476dfe
Haleigh Spurlock <haleighspurlock@hotmail.com>
2021-02-23 20:44:52 -0500

    html working and looking good

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,7 @@
- 
+// oneLocation - api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+// 5day - api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
+// current UV - http://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid={API key}
+
+
+
+var apiKey = '49283df107f4b9c05123a0013d52be80';
