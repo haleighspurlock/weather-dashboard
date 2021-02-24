ac6175f03cf561383102cc4d9d9ebf08fe80f3c1
Haleigh Spurlock <haleighspurlock@hotmail.com>
2021-02-24 18:27:53 -0500

    populating information from api, updated html/js, updated README

---
+++ README.md
@@ -0,0 +1,52 @@
+# **weather-dashboard(“weather-dashboard”)**
+
+## Table of Contents
+
+* [Description](#description)
+* [Requirements](#requirements)
+* [Installation](#installation)
+* [Contributing](#contributing)
+* [Credits](#credits)
+
+## Description
+---
+UPDATE DISCRIPTION HERE
+
+Deployed Website: 
+
+![Work Day Scheduler](assets/work-day-schedule-pic.png)
+![Work Day Scheduler](assets/local-storage-pic.png)
+
+## Requirements
+---
+```
+GIVEN a weather dashboard with form inputs
+WHEN I search for a city
+THEN I am presented with current and future conditions for that city and that city is added to the search history
+WHEN I view current weather conditions for that city
+THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+WHEN I view the UV index
+THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+WHEN I view future weather conditions for that city
+THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
+WHEN I click on a city in the search history
+THEN I am again presented with current and future conditions for that city
+```
+
+## Installation
+---
+No installation needed for this project
+
+See deployed site here: https://haleighspurlock.github.io/weather-dashboard
+
+See repository here: https://github.com/haleighspurlock/weather-dashboard
+## Contributing
+---
+
+Any and all improvement suggestions are welcome! 
+
+Email: haleighspurlock@gmail.com
+
+## Credits
+---
+© 2021 Haleigh Spurlock
--- assets/script.js
+++ assets/script.js
@@ -5,9 +5,53 @@
 
 
 var apiKey = '49283df107f4b9c05123a0013d52be80';
+var date = moment();
+var formatDate = date.format('MM/DD/YYYY');
 
 function populateWeatherData(){
     get5DayForcast()
+    getOneLocationForcast()
+    getIndex()
+}
+
+function getIndex(lat, long) {
+    var requestUrl = `http://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${long}&appid=${apiKey}`
+    function makeRequest(indexResponse){
+        console.log(indexResponse)
+    }
+    $.ajax({
+        url: requestUrl,
+        method: 'GET',
+    }).then(makeRequest)
+}
+
+function getOneLocationForcast() {
+    var searchInput = $('#searchCity')
+    var requestUrl = `http://api.openweathermap.org/data/2.5/weather?q=${searchInput.val()}&appid=${apiKey}`
+
+    function populateOneDayForcast(dailyForcastInfo){
+        // var uvIndex = getIndex(dailyForcastInfo.coord.lat, dailyForcastInfo.coord.lon)
+        // var dailyCityWeather = $('#dailyCityWeather');
+        // var element = `
+        //     <h3><b>${dailyForcastInfo.name} ${formatDate}</b><img class='icon' src='./assets/images/sunnyicon.svg'/></h3>
+        //     <p>${dailyForcastInfo.main.temp}</p>
+        //     <p>${dailyForcastInfo.main.humidity}</p>
+        //     <p>${dailyForcastInfo.wind.speed}</p>
+        //     <p>UV Index: <span id='uvIndexCondition'>${uvIndex}</span> </p>
+        //     `
+        // dailyCityWeather.append(element);
+        // return dailyForcastInfo
+        $('#name').text(dailyForcastInfo.name)
+        $('#temp').text(dailyForcastInfo.main.temp)
+        $('#date').text(formatDate)
+        $('#humidity').text(dailyForcastInfo.main.humidity)
+        $('#windspeed').text(dailyForcastInfo.wind.speed)
+        $('#icon').val(dailyForcastInfo.weather.icon)
+    }
+    $.ajax({
+        url: requestUrl,
+        method: 'GET',
+    }).then(populateOneDayForcast)
 }
 
 function get5DayForcast(){
@@ -15,26 +59,29 @@ function get5DayForcast(){
     var requestUrl = `http://api.openweathermap.org/data/2.5/forecast?q=${searchInput.val()}&appid=${apiKey}`
 
     function populate5DayForcast(weekForcastInfo){
+        var forcastSpace = $('#forcastSpace');
+        forcastSpace.empty();
         var dailyForcastHour = []
         var pastDay = -1;
         for (var weatherHour of weekForcastInfo.list) {
-            var day = moment.unix(weatherHour.dt).day();
+            var day = moment(weatherHour.dt_txt).dayOfYear();
             if (day !== pastDay) {
                 dailyForcastHour.push(weatherHour)
                 pastDay = day   
             }
-            var forcastSpace = $('#forcastSpace');
+        }
+        for (var weatherHour of dailyForcastHour){
             var element = `
             <div class="card col-2 forcast-day-card" style="width: 18rem;">
                 <div class='card-body p-1'>
-                    <h6 class='card-title'>8/16/2019</h6>
-                    <img class='card-img icon' src='./assets/images/sunnyicon.svg'/>
-                    <p class='card-text'>Temp:86.84 &deg;F</p>
-                    <p class='card-text'></p>
+                    <h6 class='card-title'>${moment(weatherHour.dt_txt).format('MM/DD/YYYY')}</h6>
+                    <img class='card-img icon' ${weatherHour.weather.icon}/>
+                    <p class='card-text'>Temp:${weatherHour.main.temp}&deg;F</p>
+                    <p class='card-text'>${weatherHour.main.humidity}</p>
                 </div>
             </div>
             `
-            forcastSpace.append(element);
+        forcastSpace.append(element);
         }
     }
     $.ajax({
